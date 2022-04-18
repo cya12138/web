598658a59565e2392cefba2ae2c30da9ab530ca1
cya12138 <78086834+cya12138@users.noreply.github.com>
2022-04-18 17:38:31 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -11,121 +11,33 @@ from pyecharts.charts import Map, Bar, Grid
 from pyecharts import options as opts
 import matplotlib.image as mpimg
 
-data=pd.read_excel('data异常.xlsx')
-data['ITEM_PRICE']=data['ITEM_PRICE'].apply(lambda x: x)
-def tranAge(x):
-   if 0<=x<100:
-      x = 1
-   elif 100<=x<1000:
-      x = 2
-   elif 1000<=x<10000:
-      x = 3
-   else :
-      x =0
-   return x
-data['ITEM_PRICE'] = data['ITEM_PRICE'].apply(tranAge)
-price = data.ITEM_PRICE.value_counts()
-price=pd.DataFrame(price)
-price.index=['0-100','100-1000','1000-10000' ,'大于10000']
-price["number"]=price["ITEM_PRICE"]
-price=price.drop(columns=['ITEM_PRICE'])
-miss = data.miss.value_counts()
-miss=pd.DataFrame(miss)
-miss=miss.reset_index()
-data['ITEM_SALES_VOLUME']=data['ITEM_SALES_VOLUME'].apply(lambda x: x)
-def tranAge(x):
-   if 0<=x<100:
-      x = 1
-   elif 100<=x<1000:
-      x = 2
-   elif 1000<=x<10000:
-      x = 3
-   else :
-      x =0
-   return x
-data['ITEM_SALES_VOLUME'] = data['ITEM_SALES_VOLUME'].apply(tranAge)
-volume=data.ITEM_SALES_VOLUME.value_counts()
-volume=pd.DataFrame(volume)
-volume["volume"]=["0-100","100-1000","1000-10000","大于10000"]
-def tranAge(x):
-   if 0<=x<100:
-      x = 1
-   elif 100<=x<1000:
-      x = 2
-   elif 1000<=x<10000:
-      x = 3
-   elif 10000<=x<100000:
-      x = 4
-   elif 10000<=x<100000:
-      x=5
-   else:
-      x=0
-   return x
-data['ITEM_SALES_AMOUNT'] = data['ITEM_SALES_AMOUNT'].apply(tranAge)
-AMOUNT= data.ITEM_SALES_AMOUNT.value_counts()
-AMOUNT=pd.DataFrame(AMOUNT)
-AMOUNT=AMOUNT.reset_index()
-data.CATE_NAME_LV1= data.CATE_NAME_LV1.replace({np.nan:'missing'})
-cate= data.CATE_NAME_LV1.value_counts()
-cate=pd.DataFrame(cate)
-cate=cate.reset_index()
-y1 = np.array(cate['CATE_NAME_LV1'])
-y1 =y1.tolist()
-x = np.array(cate['index'])
-x =x.tolist()
-data.ITEM_DELIVERY_PLACE= data.ITEM_DELIVERY_PLACE.replace({np.nan:'missing'})
-PLACE= data.ITEM_DELIVERY_PLACE.value_counts()
-PLACE=pd.DataFrame(PLACE)
-PLACE=PLACE.reset_index()
-PLACE=PLACE.drop([0])
-province_list = PLACE['index'].tolist()#将省份数据提取成列表
-d_data = PLACE['ITEM_DELIVERY_PLACE'].values.tolist()#将确证的人数提取成列表
-d=[]
-for p in province_list:#构建循环
-    n=d_data[province_list.index(p)]#根据列表中的省份名提取对应的确诊人数。
-    p = p.replace('省', '').replace('市', '').replace('自治区', '').replace('壮族', '').replace('维吾尔', '').replace('回族', '')#将省份名改成适用于map的省份名。
-    d.append([p, n])#将
-def tranAge(x):
-   if 0<=x<100:
-      x = 1
-   elif 100<=x<1000:
-      x = 2
-   elif 1000<=x<10000:
-      x = 3
-   elif x>10000:
-      x =4
-   return x
-data['ITEM_FAV_NUM'] = data['ITEM_FAV_NUM'].apply(tranAge)
-data.ITEM_FAV_NUM= data.ITEM_FAV_NUM.replace({np.nan:'missing'})
-ITEM_FAV_NUM=data.ITEM_FAV_NUM.value_counts()
-ITEM_FAV_NUM=pd.DataFrame(ITEM_FAV_NUM)
-ITEM_FAV_NUM=ITEM_FAV_NUM.reset_index()
-def tranAge(x):
-   if 0<=x<100:
-      x = 1
-   elif 100<=x<1000:
-      x = 2
-   elif 1000<=x<10000:
-      x = 3
-   elif x>10000:
-      x =4
-   return x
-data['TOTAL_EVAL_NUM'] = data['TOTAL_EVAL_NUM'].apply(tranAge)
-data.TOTAL_EVAL_NUM= data.TOTAL_EVAL_NUM.replace({np.nan:'missing'})
-TOTAL_EVAL_NUM=data.TOTAL_EVAL_NUM.value_counts()
-TOTAL_EVAL_NUM=pd.DataFrame(TOTAL_EVAL_NUM)
-TOTAL_EVAL_NUM=TOTAL_EVAL_NUM.reset_index()
-ITEM_FAV_NUM_y1 = np.array(ITEM_FAV_NUM['ITEM_FAV_NUM'])#将男性转换成numpy类型的数据
-ITEM_FAV_NUM_y1 =ITEM_FAV_NUM_y1.tolist()#
-TOTAL_EVAL_NUM_y2 = np.array(TOTAL_EVAL_NUM['TOTAL_EVAL_NUM'])#将男性转换成numpy类型的数据
-TOTAL_EVAL_NUM_y2 =TOTAL_EVAL_NUM_y2.tolist()#
-FAV_EVAL_x=["missing","0-100","100-1000","1000-10000","大于10000"]
 
 
+@st.cache(allow_output_mutation=True)
+def read():
+   data=pd.read_excel('D:\pythonweb\data异常.xlsx')
+   return data
+data=read()
 
-
-
+data1=data.copy()
 def volume_number():
+    data1['ITEM_SALES_VOLUME'] = data1['ITEM_SALES_VOLUME'].apply(lambda x: x)
+
+    def tranAge(x):
+        if 0 <= x < 100:
+            x = 1
+        elif 100 <= x < 1000:
+            x = 2
+        elif 1000 <= x < 10000:
+            x = 3
+        else:
+            x = 0
+        return x
+
+    data1['ITEM_SALES_VOLUME'] = data1['ITEM_SALES_VOLUME'].apply(tranAge)
+    volume = data1.ITEM_SALES_VOLUME.value_counts()
+    volume = pd.DataFrame(volume)
+    volume["volume"] = ["0-100", "100-1000", "1000-10000", "大于10000"]
     funnel1= (Funnel()
               .add("用户数", [list(z) for z in zip(volume['volume'], volume['ITEM_SALES_VOLUME'])],
                    # 生成[年龄结构,数据]的列表 ，并放进图标
@@ -137,6 +49,25 @@ def volume_number():
 
 
 def amount():
+    def tranAge(x):
+        if 0 <= x < 100:
+            x = 1
+        elif 100 <= x < 1000:
+            x = 2
+        elif 1000 <= x < 10000:
+            x = 3
+        elif 10000 <= x < 100000:
+            x = 4
+        elif 10000 <= x < 100000:
+            x = 5
+        else:
+            x = 0
+        return x
+
+    data1['ITEM_SALES_AMOUNT'] = data1['ITEM_SALES_AMOUNT'].apply(tranAge)
+    AMOUNT = data1.ITEM_SALES_AMOUNT.value_counts()
+    AMOUNT = pd.DataFrame(AMOUNT)
+    AMOUNT = AMOUNT.reset_index()
     list = ['100-1000', '0-100', '1000-10000', '10000-100000', "大于100000"]
     fig, ax = plt.subplots(figsize=(16,10), facecolor='white', dpi= 80)
     ax.vlines(x=AMOUNT.index, ymin=0, ymax=AMOUNT.ITEM_SALES_AMOUNT, color='blue', alpha=0.7, linewidth=40)  # 画好辅助线
@@ -179,6 +110,14 @@ def delivery():
 
 
 def cate():
+    data1.CATE_NAME_LV1 = data1.CATE_NAME_LV1.replace({np.nan: 'missing'})
+    cate = data1.CATE_NAME_LV1.value_counts()
+    cate = pd.DataFrame(cate)
+    cate = cate.reset_index()
+    y1 = np.array(cate['CATE_NAME_LV1'])
+    y1 = y1.tolist()
+    x = np.array(cate['index'])
+    x = x.tolist()
     bar = (Bar()  # 定义一个条形图
            .add_xaxis(x)  # 加上我们的年份数据
            .add_yaxis('数量', y1)
@@ -192,6 +131,9 @@ def cate():
 
 
 def miss_number():
+    miss = data1.miss.value_counts()
+    miss = pd.DataFrame(miss)
+    miss = miss.reset_index()
     fig, ax = plt.subplots(figsize=(16, 10), facecolor='white', dpi=80)
     ax.bar([i + 1 for i in range(14)], miss["miss"], width=0.5)
     ax.set_xlabel("缺失数量")  # 设置x轴标签
@@ -206,6 +148,25 @@ def miss_number():
 
 
 def price_number():
+    data1['ITEM_PRICE'] = data1['ITEM_PRICE'].apply(lambda x: x)
+
+    def tranAge(x):
+        if 0 <= x < 100:
+            x = 1
+        elif 100 <= x < 1000:
+            x = 2
+        elif 1000 <= x < 10000:
+            x = 3
+        else:
+            x = 0
+        return x
+
+    data1['ITEM_PRICE'] = data1['ITEM_PRICE'].apply(tranAge)
+    price = data1.ITEM_PRICE.value_counts()
+    price = pd.DataFrame(price)
+    price.index = ['0-100', '100-1000', '1000-10000', '大于10000']
+    price["number"] = price["ITEM_PRICE"]
+    price = price.drop(columns=['ITEM_PRICE'])
     fig = px.pie(price ,values='number', names=['0-100', '100-1000', '1000-10000', '大于10000'],
                  color_discrete_map={
                      '0-100': 'lightcyan',
@@ -218,6 +179,44 @@ def price_number():
 
 
 def FAV_EVA():
+    def tranAge(x):
+        if 0 <= x < 100:
+            x = 1
+        elif 100 <= x < 1000:
+            x = 2
+        elif 1000 <= x < 10000:
+            x = 3
+        elif x > 10000:
+            x = 4
+        return x
+    data1['ITEM_FAV_NUM'] = data1['ITEM_FAV_NUM'].apply(tranAge)
+    data1.ITEM_FAV_NUM = data1.ITEM_FAV_NUM.replace({np.nan: 'missing'})
+    ITEM_FAV_NUM = data1.ITEM_FAV_NUM.value_counts()
+    ITEM_FAV_NUM = pd.DataFrame(ITEM_FAV_NUM)
+    ITEM_FAV_NUM = ITEM_FAV_NUM.reset_index()
+
+    def tranAge(x):
+        if 0 <= x < 100:
+            x = 1
+        elif 100 <= x < 1000:
+            x = 2
+        elif 1000 <= x < 10000:
+            x = 3
+        elif x > 10000:
+            x = 4
+        return x
+
+    data1['TOTAL_EVAL_NUM'] = data1['TOTAL_EVAL_NUM'].apply(tranAge)
+    data1.TOTAL_EVAL_NUM = data1.TOTAL_EVAL_NUM.replace({np.nan: 'missing'})
+    TOTAL_EVAL_NUM = data1.TOTAL_EVAL_NUM.value_counts()
+    TOTAL_EVAL_NUM = pd.DataFrame(TOTAL_EVAL_NUM)
+    TOTAL_EVAL_NUM = TOTAL_EVAL_NUM.reset_index()
+    ITEM_FAV_NUM_y1 = np.array(ITEM_FAV_NUM['ITEM_FAV_NUM'])  # 将男性转换成numpy类型的数据
+    ITEM_FAV_NUM_y1 = ITEM_FAV_NUM_y1.tolist()  #
+    TOTAL_EVAL_NUM_y2 = np.array(TOTAL_EVAL_NUM['TOTAL_EVAL_NUM'])  # 将男性转换成numpy类型的数据
+    TOTAL_EVAL_NUM_y2 = TOTAL_EVAL_NUM_y2.tolist()  #
+    FAV_EVAL_x = ["missing", "0-100", "100-1000", "1000-10000", "大于10000"]
+
     bar = (Bar()  # 定义一个条形图
            .add_xaxis(FAV_EVAL_x)  # 加上我们的年份数据
            .add_yaxis('收藏', ITEM_FAV_NUM_y1, itemstyle_opts=opts.ItemStyleOpts(color="orange"))  # 加上男性人口的数据
@@ -234,11 +233,10 @@ def FAV_EVA():
 def Double_coordinates():
 
     st.markdown('#### 数据表展示')
-    data[0:1000]
-    data['ITEM_FAV_NUM'] = data['ITEM_FAV_NUM'].astype("str")
-    data['TOTAL_EVAL_NUM'] = data['TOTAL_EVAL_NUM'].astype("str")
-    data['ITEM_STOCK'] = data['ITEM_STOCK'].astype("str")
-    st.table(data)
+    data1=data.head(100)
+    data1.drop(columns=["Unnamed: 0","Unnamed: 0.1"])
+    st.table(data1)
+
     #
     # st.markdown('#### 双坐标图')
     # x = df["年月"]
@@ -296,7 +294,7 @@ def Layouts_plotly():
     elif add_selectbox == "发货地":
         delivery()
     # 补充表单
-    st.sidebar.button('基本数据表', on_click=Double_coordinates)
+    st.sidebar.button('异常数据表', on_click=Double_coordinates)
 
 
 def main():
